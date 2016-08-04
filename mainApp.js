54d5b5eae49e44afe710f18a6d55027f10347811
DavidAndrianne <r0426942@gmail.com>
2016-08-04 17:21:27 +0200

    - Removed icons from overview table - Added redirect to Quandl's awful dataset searching "API" :(

--- mainApp.js
+++ mainApp.js
@@ -11,6 +11,10 @@
 	app.factory('quandl', function($resource){
 		return $resource('https://www.quandl.com/api/v3/databases.json'); //declare a REST service with the ng-resource base functions
 	});
+	// Create a new angular service 'datasets' for querying quandl datasets
+	/*app.factory('datasets', function($resource){
+		return $resource('https://www.quandl.com/api/v3/datasets'); //example query : ?database_code=YFinance&include%5B%5D=latest_values&include%5B%5D=related_datasets&page=1&per_page=20&query=AT%26
+	});*/
 	
 	// Create a directive for a database
 	app.directive('database', function(){
@@ -26,19 +30,43 @@
 	});
 	
 	// Add angular controller 'mainCtrl'
-	app.controller('mainCtrl', function($scope, quandl, api_key){
+	app.controller('mainCtrl', function($scope, $location, quandl, /*datasets,*/ api_key){
 		//init values
 		$scope.filterPremium = false;
 		$scope.query_databases = "Stock";
 		
+		/*
+		 * Updates the list of available databases
+		 */
 		$scope.updateTable = function(){
-			quandl.get({query: $scope.query_databases, api_key : api_key}, function(response){
+			quandl.get({query: $scope.query_databases, api_key: api_key}, function(response){
 				$scope.databases = response.databases;
 			});
-		}
+		};
 		
+		/*
+		 * Views a database in detail with it's icon, description and subsequent datasets
+		 */
 		$scope.viewDatabase = function(database){
 			$scope.detailDB = database;
+			//$scope.updateDatasets(); // update datasets for selected database
+		};
+		
+		/*
+		 * Updates the datasets for the selected database and specified querystring
+		 */
+		/*$scope.updateDatasets = function(){
+			datasets.query({database_code:$scope.detailDB.name, query: $scope.datasetQuery}, function(datasets){
+				$scope.datasets = datasets;
+			});
+		};*/
+		
+		/*
+		 * This function redirects the user to the specified url
+		 */
+		$scope.redirect = function(url){
+			//$location.url(url); 
+			window.location = url; // $location.url doesn't work for local testing
 		};
 		
 		$scope.updateTable();
